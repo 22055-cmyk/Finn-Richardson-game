1513ef94d36a198c0f28ab3c03ad785522106bf5
22055-cmyk <22055@fraser.school.nz>
2026-02-28 21:36:49 +1300

    creating a button for the main menu

--- fastmode.py
+++ fastmode.py
@@ -1,3 +1,42 @@
+import sys
 import pygame
+import random
+
+
 pygame.init()
 
+fps = 60
+fpsClock = pygame.time.Clock()
+
+window_width = 1000
+window_height = 750
+red = (255, 0, 0)
+black = (0, 0, 0)
+width = 100
+height = 100
+
+
+screen = pygame.display.set_mode((window_width, window_height))
+screen.fill(black)
+font = pygame.font.SysFont('Arial', 40)
+button_onscreen = pygame.Rect(((window_width - width)/2, (window_height - height)/2), (width, height))
+
+
+
+gameloop = True
+while gameloop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameloop = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if button.collidepoint(pos):
+                print('hit')
+                gameloop = False
+            else:
+                print('no hit')
+    button = pygame.draw.rect(screen, red, button_onscreen)
+    pygame.display.update()
+
+
+pygame.quit
