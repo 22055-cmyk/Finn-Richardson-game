18b27d2b00035a742f95edf1eaf81f90190f3057
22055-cmyk <22055@fraser.school.nz>
2026-03-03 14:55:18 +1300

    FIXED CUSTOM BUTTON COLOURS YAY
    had to use self.rect_colour.get(colours)

--- fastmode.py
+++ fastmode.py
@@ -10,14 +10,6 @@ fpsClock = pygame.time.Clock()
 
 window_width = 1000
 window_height = 750
-red = (255, 0, 0)
-black = (0, 0, 0)
-white = (255, 255, 255)
-green = (0, 255, 0)
-yellow = (255, 255, 0)
-blue = (0, 0, 255)
-purple = (128, 0, 128)
-orange = (255, 165, 0)
 screen = pygame.display.set_mode((window_width, window_height))
 font = pygame.font.Font(None, 36)
 
@@ -25,8 +17,9 @@ font = pygame.font.Font(None, 36)
 class Button:
     """this code creates a button blueprint
     
-    this code does hcdfhjgxchjghxfgxghfgdfgfgxfcgdfgdfgdfgdfgdfgdfsgdstgdsgdsgds
-
+    This code makes a button that can have custom, x and y position, size, what is displayed on it, what happens when its pressed and the colours you want to use.
+    I will use this code to make buttons for my games menus, which will have lots of different colours, functions and looks.
+    I am using this code because it makes the process of making buttons very easy because I don't have to retype all this code everytime I want a new button.
 
     thus code changes
     -cghdfhdfghdg
@@ -34,29 +27,47 @@ class Button:
     -gfjdf-hgfhdhcgj
     
     """
-    rect_colour = yellow
-    text_colour = black
-    onclickFunction = None
-    def __init__(self, rect_colour, text_colour, onclickFunction, x, y, width, height, text):
+    # A class attribute dictionary, of the colours available
+    colours = {
+            'red': '#FF0000',
+            'black': '#000000',
+            'white': '#FFFFFF',
+            'green': '#008000',
+            'yellow': '#FFFF00',
+            'blue': '#0000FF',
+            'purple': '#9D00FF',
+            'orange': '#FFA500',
+        }
+    def __init__(self, x, y, width, height, text, onclickFunction, rect_colour, text_colour):
+        #Gets the dimensions for the button size and position
         self.rect = pygame.Rect(x, y, width, height)
+        #Text on the button
         self.text = text
-        self.image.fill(rect_colour) 
-        self.rect = self.image.get_rect()
+        #These lines of code check whether you have defined a colour from the dictionary if so it will set the colour of the rect or text to the definition
+        self.rect_colour = self.colours.get(rect_colour)
+        self.text_colour = self.colours.get(text_colour)
+        self.rect_colour = rect_colour
         self.text_colour = text_colour
+        #This defines what will happen when the button is clicked
         self.onclickFunction = onclickFunction
 
     def draw(self, surface):
+        #This line draws the rect using what surface to put it on, defined when in the main loop, the colour and dimensions defined when using the class
         pygame.draw.rect(surface, self.rect_colour, self.rect)
         text_surface = font.render(self.text, True, self.text_colour)
         text_rect = text_surface.get_rect(center = self.rect.center)
         surface.blit(text_surface, text_rect)
 
     def check_click(self, pos):
+        #Checks if mouse is over the button
         if self.rect.collidepoint(pos):
+            #Checks whether onclickfunction has been defined in the class blueprint
             if self.onclickFunction:
+                #Then it does the function defined below
                 self.onclickFunction()
-
+#These are the definitions 
 def exit():
+    #If you use exit function then the game will exit.
     pygame.quit()
     sys.exit()
 
@@ -82,8 +93,8 @@ def play():
 #screen.blit(button_text, text_rect)
 
 
-ExitButton = Button(100, 100, 100, 100, "exit", exit, red, white)
-PlayButton = Button(300, 300, 100, 100, 'play', play, green, white)
+ExitButton = Button(((window_width - 100)), ((window_height - 60)/2), 100, 60, "exit", exit, 'red', 'white')
+PlayButton = Button(300, ((window_height - 60)/2), 100, 60, 'play', play, 'green', 'white')
 
 startmenu = True
 while startmenu:
